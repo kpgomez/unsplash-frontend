90d788036965f0b4c2048d3a42a4485ef2e977f0
Kiengchay Gomez <kphoutthavong@gmail.com>
2023-05-12 23:06:38 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import axios from 'axios';
+import { Button, Container, Form } from 'react-bootstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userInput: '',
+      photoData: []
+    }
+  }
+
+  updateInput = (e) => this.setState({ userInput: e.target.value }, () => console.log(this.state.userInput));
+
+  handleSearchPhoto = async (e) => {
+    e.preventDefault();
+    const url = `${process.env.REACT_APP_SERVER}/photos?searchQuery=${this.state.userInput}`;
+    console.log(url);
+    const response = await axios.get(url);
+    this.setState({ photoData: response.data });
+    console.log(response.data);
+  }
+
+  render() {
+    return (
+      <>
+        <h1>Unsplash API</h1>
+        <Container>
+          <Form onSubmit={this.handleSearchPhoto}>
+            <Form.Group controlId="formUnsplash">
+              <Form.Label>Search any image from the Unsplash Database</Form.Label>
+              <Form.Control type="text" onChange={this.updateInput} />
+            </Form.Group>
+            <Button type="submit">Search</Button>
+          </Form>
+        </Container>
+        {this.state.photoData.length > 0 &&
+          this.state.photoData.map(photo =>
+            <>
+              <img src={photo.imgURL} alt={photo.portfolio} />
+            </>
+          )
+        }
+      </>
+    )
+  }
 }
-
 export default App;
